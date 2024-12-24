4f6d7ca4a147dab130016749f6d6600416b4a65f
Santa_Robert <santa.robert.2011@gmail.com>
2024-12-24 14:24:14 +0200

    Added a new tab in the table for Emails

--- OperationJinx/Tablazat.cpp
+++ OperationJinx/Tablazat.cpp
@@ -6,10 +6,12 @@ void data(string name ,
               string money ,
               string age ,
               string gender,
+              string email,
               int countName,
               int countMoney,
               int countAge,
-              int countGender
+              int countGender,
+              int countEmail
 );
 
 
@@ -22,6 +24,7 @@ int userTable() {
     string money = "Money";
     string age = "Age";
     string gender = "Gender";
+    string email = "Email";
 
     cout<<name;
     for (int i = 0; i < n; i++) {
@@ -50,13 +53,28 @@ int userTable() {
     cout<<"Gender";
     for (int i = 0; i < n; i++) {
         cout<<" ";
-
+        if (i+1 == 8)
+        {
+            cout<<"|";
+            break;
+        }
     }
+    cout<<"Email";
+    for (int i = 0; i < n; i++) {
+        cout<<" ";
+        if(i+1 == 35)
+        {
+            cout<<"|";
+            break;
+        }
+    }
+    
     cout<<endl;
     int nameCount = 0;
     int moneyCount = 0;
     int ageCount = 0;
     int genderCount = 0;
+    int emailCount = 0;
     for (int i = 0; i < n; i++) {
         cout<<"-";
         nameCount++;
@@ -84,15 +102,26 @@ int userTable() {
     for (int i = 0; i < n; i++) {
         cout<<"-";
         genderCount++;
+        if (i+1 == gender.size() + 8)
+        {
+            cout<<"+";
+            break;
+        }
     }
+    for (int i = 0; i < n; i++) {
+        cout<<"-";
+        emailCount++;
+        
+    }
+    cout<<"+";
 
-
+    
     cout<<endl;
-    data("Paul Eminescu", "12", "61", "Male", nameCount, moneyCount, ageCount, genderCount);
-    data("Roby", "152396489", "13", "Male", nameCount, moneyCount, ageCount, genderCount);
-    data("Lenard Mate Istvan", "51233584", "26", "Programmer", nameCount, moneyCount, ageCount, genderCount);
-    data("Julia", "1235", "5", "femlae", nameCount, moneyCount, ageCount, genderCount);
-
+    data("Paul Eminescu", "12", "61", "Male","sophiataylor@gmail.com", nameCount, moneyCount, ageCount, genderCount,emailCount);
+    data("Paul Jake", "12", "61", "Male","danielanderson@gmail.com", nameCount, moneyCount, ageCount, genderCount,emailCount);
+    data("Rob", "12", "61", "Male","jameswilson@gmail.com", nameCount, moneyCount, ageCount, genderCount,emailCount);
+    data("Huber Blaine Wolf", "12", "61", "Male","michaelbrown@gmail.com", nameCount, moneyCount, ageCount, genderCount,emailCount);
+  
     return 1;
 
 }
@@ -101,10 +130,12 @@ void data(string name ,
               string money ,
               string age ,
               string gender,
+              string email,
               int countName,
               int countMoney,
               int countAge,
-              int countGender) {
+              int countGender,
+              int countEmail) {
 
     int n = 40;
 
@@ -136,9 +167,19 @@ void data(string name ,
     cout<<gender;
     for (int i = 0; i < n; i++) {
         cout<<" ";
-
+        if(i+1 == countGender - gender.size()){
+            cout<<"|";
+            break;
+        }
     }
-
+    cout<<email;
+    for (int i = 0; i < n; i++) {
+        cout<<" ";
+        if(i+1 == countEmail - email.size()){
+            cout<<"|";
+            break;
+        }
+    }
     cout<<endl;
 
 }
